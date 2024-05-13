2f43d3c1c868b2c0c4bff78833835cb13ab8d570
Adetth <raju.adetth@gmail.com>
2024-05-13 22:19:24 +0530

    Potrait Warning added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Farewell from './components/farewell';
 
 const App = () => {
+  if(window.innerHeight > window.innerWidth){
+    alert("Please rotate to get the best experience!");
+}
   return (
     <div>
       <Farewell />
