b68a1a951c79a5194c7694de4d6470be287879fb
Adetth <raju.adetth@gmail.com>
2024-05-13 22:50:29 +0530

    Contribute fixed?

--- src/components/farewell.js
+++ src/components/farewell.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import '../App.css';
 import '../index.css'
-import giphy5 from './giphy5.gif'
+import { useNavigate } from 'react-router-dom';
 import Form from './Form';
 
 const Farewell = () => {
@@ -53,15 +53,15 @@ function setOpacity() {
       div1.style.opacity = "1";
     }
 }
-
-const redirectToForm = () => {
-    window.location.href = 'https://forms.gle/P5UA25T2sw1FvS7F9'; // Replace with your actual form link
+const navigate = useNavigate();
+const handleFormClick = () => {
+    navigate('https://forms.gle/P5UA25T2sw1FvS7F9'); // Replace with your form link
   };
 
     return (
     <div>
         <header className="button">
-             <button className="enterOne" onClick={redirectToForm}>Contribute</button>
+             <button className="enterOne" onClick={handleFormClick}>Contribute</button>
         </header>
         <header className="button1">
             <button className="enterTwo" onClick={handleForm}>Register</button>
